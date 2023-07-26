cdc7bec937846ca1bcad233154183a7471565433
John Wilken Christopher <92281847+jhnwlkn@users.noreply.github.com>
2023-07-26 21:15:50 +0700

    Add files via upload

---
+++ CPP_Flow.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Branching (if-else)
+    // int angka; // deklarasi variabel angka dengan tipe data integer
+    // cout << "Masukan sebuah angka: "; // output
+    // cin >> angka; //input
+    
+    // cout << "angka nilainya: " << angka << endl;
+
+    // if(angka < 0){
+    //     cout << "angka ini negatif";
+    // } else if(angka > 0){
+    //     cout << "angka ini positif";
+    // } else {
+    //     cout << "angka ini 0";
+    // }
+
+
+    // Branchine (switch case)
+    // int menu;
+    // cout << "1. Kopi" << endl;
+    // cout << "2. Susu" << endl;
+    // cout << "Silahkan pilih menu: " << endl;
+    // cin >> menu;
+
+    // switch (menu){ // menu disini sebagai ekspresi
+    // case 1: //case 1
+    //     cout << "Silahlkan nikamtin kopinya";
+    //     break; // untuk memberhentikan switch case agar hanya mengeluarkan output di case 1 dan tidak yang lain.
+    // case 2: // case 2
+    //     cout << "Silahkan nikmatin susunya";
+    //     break;
+    // default: // apabila kedua case di atas tidak di jalankan, maka default default akan di jalankan
+    //     cout << "Pilihan anda tidak tersedia";
+    //     break;
+    // }
+
+    // const int nilai = 90;
+    // cout << nilai << endl;
+    // nilai = 70;
+    // cout << nilai;
+
+    // Looping
+    // While loop
+    // int limit = 1;
+    // while(limit != 1){
+    //     cout << "Hello world" << endl;
+    //     limit++; // limit = limit sebelumnya + 1
+    // }
+
+    // Do-While loop
+    // do{
+    //     cout << "Hello World" << endl;
+    // }while(limit != 1);
+
+    // For loop
+    // for(int i=0; i<10; i++){
+    //     cout << "Hello world" << endl;
+    // }
+
+    // Tebak angka
+    // int angka = 7;
+    // int tebakan;
+    // while(tebakan != angka){
+    //     cout << "Masukan tebakan anda: ";
+    //     cin >> tebakan;
+    // }
+
+    return 0;
+}
